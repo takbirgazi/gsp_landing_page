918d8ad7ca8efd17e5a4051445eaa206aa697f95
takbirgazi <takbirgazibd@gmail.com>
2024-08-26 16:39:32 +0600

    Update Slider Carusal

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,43 +1,17 @@
-import { useState } from "react";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { Carousel } from "flowbite-react";
+import './Hero.css';
 
 function Hero() {
-    const [count, setCount] = useState(1);
-    const [image, setImage] = useState(<img className="h-full w-full object-cover transition-all ease-in delay-[5000ms] origin-right" src={`./src/assets/images/sliderImage-${count}.jpg`} />);
-    const length = 4;
-
-    const handlerPrev = (count) => {
-        if (count <= 1) {
-            setCount(length);
-        } else {
-            setCount(--count);
-        }
-        setImage(<img className="h-full w-full object-cover ease-in delay-[5000ms] origin-right" src={`./src/assets/images/sliderImage-${count}.jpg`} />);
-    }
-
-    const handlerNext = (count) => {
-        setImage(<img className="h-full w-full object-cover ease-in delay-[5000ms] origin-right" src={`./src/assets/images/sliderImage-${count}.jpg`} />);
-        if (count >= length) {
-            setCount(1);
-        } else {
-            setCount(++count);
-        }
-    }
-
-    setInterval(() => {
-        handlerNext(count);
-    }, 5000);
 
     return (
-        <header className={`text-white h-screen flex items-center justify-center transition-all`}>
-            <span onClick={() => handlerPrev(count)} className="text-2xl absolute left-10 z-10 cursor-pointer border rounded-full h-10 w-10 p-2 hidden items-center justify-center hover:bg-gray-200 hover:text-black"> <FaArrowLeft /> </span>
-
-            <div className="h-full w-full">
-                {image}
-            </div>
-
-            <span onClick={() => handlerNext(count)} className="text-2xl absolute right-10 z-10 cursor-pointer border rounded-full h-10 w-10 p-2 hidden items-center justify-center hover:bg-gray-200 hover:text-black "> <FaArrowRight /> </span>
-        </header>
+        <div className="h-56 md:h-72 lg:h-[450px] flex items-center justify-center mb-10">
+            <Carousel className="mt-32">
+                <img className="h-full" src="./src/assets/images/sliderImage-1.jpg" alt="..." />
+                <img className="h-full" src="./src/assets/images/sliderImage-2.jpg" alt="..." />
+                <img className="h-full" src="./src/assets/images/sliderImage-3.jpg" alt="..." />
+                <img className="h-full" src="./src/assets/images/sliderImage-4.jpg" alt="..." />
+            </Carousel>
+        </div>
     );
 }
 
