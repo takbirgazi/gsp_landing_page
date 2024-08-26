f100a9e07f7c08f35fc535351ed7101503adbbdc
takbirgazi <takbirgazibd@gmail.com>
2024-08-26 16:53:03 +0600

    Update slider carusal

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,20 @@
 import { Carousel } from "flowbite-react";
 import './Hero.css';
 
+import image1 from "../../assets/images/sliderImage-1.jpg";
+import image2 from "../../assets/images/sliderImage-2.jpg";
+import image3 from "../../assets/images/sliderImage-3.jpg";
+import image4 from "../../assets/images/sliderImage-4.jpg";
+
 function Hero() {
 
     return (
-        <div className="h-56 md:h-72 lg:h-[450px] flex items-center justify-center mb-10">
+        <div className="h-56 md:h-80 lg:h-[650px] flex items-center justify-center mb-10">
             <Carousel className="mt-32">
-                <img className="h-full" src="./src/assets/images/sliderImage-1.jpg" alt="..." />
-                <img className="h-full" src="./src/assets/images/sliderImage-2.jpg" alt="..." />
-                <img className="h-full" src="./src/assets/images/sliderImage-3.jpg" alt="..." />
-                <img className="h-full" src="./src/assets/images/sliderImage-4.jpg" alt="..." />
+                <img className="h-full" src={image1} />
+                <img className="h-full" src={image2} />
+                <img className="h-full" src={image3} />
+                <img className="h-full" src={image4} />
             </Carousel>
         </div>
     );
