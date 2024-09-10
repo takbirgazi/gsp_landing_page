20e0fcf4db17ab3c0283a01a891fde37957b6652
takbirgazi <takbirgazibd@gmail.com>
2024-09-10 15:20:19 +0600

    Update: project data

--- src/Pages/Dashboard/AddProject/AddProject.jsx
+++ src/Pages/Dashboard/AddProject/AddProject.jsx
@@ -1,6 +1,11 @@
+import axios from "axios";
+import { useState } from "react";
 
 
 const AddProject = () => {
+    const [allowSubmit, setAllowSubmit] = useState(false);
+    const [image, setImage] = useState(null);
+    const [successData, setSuccessData] = useState("");
     const category = [
         {
             name: `Front End Web Development`,
@@ -23,10 +28,28 @@ const AddProject = () => {
             slag: `droneSurvey`
         },
     ];
+    const handleFileInput = (e) => {
+        const formData = new FormData();
+        formData.append('image-file', e.target.files[0], e.target.files[0].name);
+        setImage(formData);
+    }
 
-    const handlerAddProject = (event) => {
+    const handlerAddProject = async (event) => {
         event.preventDefault();
-        console.log(event.target.projectCat.value)
+        const from = event.target;
+        const projName = from.projectName.value;
+        const projDsc = from.projectDesc.value;
+        const projCat = from.projectCat.value;
+        await axios.post('https://api.geosmartplanning.com.bd/api3/upload', image)
+            .then(async res => {
+                const projData = { projectName: projName, projectDescription: projDsc, projectCategory: projCat, projectImage: res.data }
+                await axios.post('http://localhost:5000/addprojects', projData)
+                    .then(res => {
+                        from.reset();
+                        setSuccessData("Project Inserted Successfully!");
+                        setAllowSubmit(false);
+                    })
+            });
     }
 
     return (
@@ -35,18 +58,19 @@ const AddProject = () => {
                 <h2 className="pb-5 pt-10 font-bold text-2xl md:text-4xl border-b-2">Add New Project</h2>
                 <div>
                     <form onSubmit={handlerAddProject} className="my-5 flex flex-col gap-3">
+                        <div className="text-green-600">{successData && successData}</div>
                         <div className="flex flex-col gap-0">
                             <label className="font-medium" htmlFor="projectName">Project Name</label>
-                            <input className="border rounded-md p-2" type="text" name="projectName" placeholder="Write Your Project Name" />
+                            <input className="border rounded-md p-2" type="text" name="projectName" placeholder="Write Your Project Name" required />
                         </div>
                         <div className="flex flex-col gap-0">
                             <label className="font-medium" htmlFor="projectDesc">Project Description</label>
-                            <textarea className="border rounded-md p-2" name="projectDesc" placeholder="Write Your Description" rows="10"></textarea>
+                            <textarea className="border rounded-md p-2" name="projectDesc" placeholder="Write Your Description" rows="10" required></textarea>
                         </div>
                         <div className="flex flex-col md:flex-row gap-2">
                             <div className="flex flex-col gap-0 flex-grow">
                                 <label className="font-medium" htmlFor="projectName">Project Category</label>
-                                <select className="border rounded-md p-2" name="projectCat" defaultValue="SelectCategory" >
+                                <select className="border rounded-md p-2" name="projectCat" defaultValue="SelectCategory" onChange={() => setAllowSubmit(true)} required >
                                     <option value="SelectCategory" disabled>Select Your Category</option>
                                     {
                                         category.map(cat => (<option key={cat.slag} value={cat.slag}>{cat.name}</option>))
@@ -55,16 +79,18 @@ const AddProject = () => {
                             </div>
                             <div className="flex flex-col gap-0">
                                 <label className="font-medium" htmlFor="projectName">Upload Your Image</label>
-                                <input className="border border-gray-800 rounded-md" accept="image/png, image/jpeg, image/jpg" type="file" name="projectImage" placeholder="Write Your Project Name" />
+                                <input className="border border-gray-800 rounded-md" accept="image/png, image/jpeg, image/jpg" type="file" name="projectImage" placeholder="Write Your Project Name" onChange={handleFileInput} required />
                             </div>
                         </div>
                         <div className="my-5">
-                            <button className="border rounded-md py-2 px-4 bg-gray-800 text-gray-200 font-bold">Submit</button>
+                            {
+                                <button className={`border rounded-md py-2 px-4 text-gray-200 font-bold ${allowSubmit ? "cursor-pointer bg-gray-800" : "cursor-not-allowed bg-gray-400"}`} disabled={allowSubmit ? false : true}>Submit</button>
+                            }
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
