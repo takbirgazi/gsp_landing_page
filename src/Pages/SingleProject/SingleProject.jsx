929045093f6052f439f6e4c7600a9bca482798ee
takbirgazi <takbirgazibd@gmail.com>
2024-09-14 13:23:25 +0600

    Update: Single project upated

--- src/Pages/SingleProject/SingleProject.jsx
+++ src/Pages/SingleProject/SingleProject.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { NavLink, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FadeLoader, ScaleLoader } from "react-spinners";
 
 
@@ -15,12 +15,14 @@ const SingleProject = () => {
     const projectId = params.projectId;
 
     axios.get(`${import.meta.env.VITE_base_url_api}/singleproject/${projectId}`)
-        .then(res => {
-            setSingleProj(res.data);
+        .then(async res => {
+            await setSingleProj(res?.data);
             setSingleProjLoading(false);
-            axios.get(`${import.meta.env.VITE_base_url_api}/allprojects/${res.data.projectCategory}`)
-                .then(data => setCatWiseProjects(data.data));
-            setCatProjLoading(false);
+            axios.get(`${import.meta.env.VITE_base_url_api}/allprojects/${res?.data?.projectCategory}`)
+                .then(async data => {
+                    await setCatWiseProjects(data?.data);
+                    setCatProjLoading(false);
+                });
         })
 
     const handlerSidebar = async (id) => {
@@ -31,17 +33,17 @@ const SingleProject = () => {
     return (
         <div className='max-w-screen-xl mx-auto pt-20'>
             <Helmet>
-                <title>{`${singleProj.projectCategory} - Geo Smart Planning`}</title>
+                <title>{`${singleProj?.projectCategory} - Geo Smart Planning`}</title>
             </Helmet>
             <div className="flex flex-col md:flex-row gap-5 m-5 ">
                 {
                     singleProjLoading ? <div className="w-full md:w-8/12 flex items-center justify-center h-96">
                         <div><FadeLoader /></div>
                     </div> : <aside className="w-full md:w-8/12 border border-gray-500 rounded-md">
-                        <img className="rounded-tl-md rounded-tr-md" src={singleProj.projectImage} alt={singleProj.projectName} />
+                        <img className="rounded-tl-md rounded-tr-md" src={singleProj?.projectImage} alt={singleProj?.projectName} />
                         <div className="p-3">
-                            <h2 className="py-2 font-bold text-2xl">{singleProj.projectName}</h2>
-                            <p>{singleProj.projectDescription}</p>
+                            <h2 className="py-2 font-bold text-2xl">{singleProj?.projectName}</h2>
+                            <p>{singleProj?.projectDescription}</p>
                         </div>
                     </aside>
                 }
@@ -51,8 +53,8 @@ const SingleProject = () => {
                     </div> : <aside className="w-full md:w-4/12">
                         <ul className="border border-gray-500 rounded p-2 flex flex-col gap-2">
                             {
-                                catWiseProjects.map(proj => <li key={proj._id} className="p-2 bg-gray-400 text-gray-100 rounded">
-                                    <button onClick={() => handlerSidebar(proj._id)}>{proj.projectName}</button>
+                                catWiseProjects.map(proj => <li key={proj?._id} className="p-2 bg-gray-400 text-gray-100 rounded">
+                                    <button onClick={() => handlerSidebar(proj?._id)} className="text-left">{proj?.projectName}</button>
                                 </li>)
                             }
                         </ul>
